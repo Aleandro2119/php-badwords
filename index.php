13f1eed2c6f85dcbeff62046ed81ef9d8a4b2dbd
Aleandro2119 <aleandro.basilico@gmail.com>
2022-03-02 18:24:12 +0100

    Add style

--- index.php
+++ index.php
@@ -22,19 +22,24 @@ $censured_length = strlen($censured);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>PHP-BADWORDS</title>
 </head>
 
 <body>
-    <main>
-        <h1><?php echo $censured ?></h1>
-        <h2><?php echo "Lunghezza paragrafo originale: $paragraph_length"; ?></h2>
-        <h2><?php echo "Lunghezza paragrafo censurato: $censured_length"; ?></h2>
+    <header class="p-4 text-center">
+        <h1>CENSURA LA PAROLA</h1>
 
         <form method="get">
-            <input type="text" placeholder="Inserisci la parola da censurare" name="word">
+            <input type="text" placeholder="Inserisci la parola da censurare..." name="word">
             <button type="submit">Censured</button>
         </form>
+    </header>
+    <main class="container text-center m-5">
+        <h2><?php echo $censured ?></h2>
+        <p><?php echo "Lunghezza paragrafo originale: $paragraph_length"; ?></p>
+        <p><?php echo "Lunghezza paragrafo censurato: $censured_length"; ?></p>
     </main>
 </body>
 
