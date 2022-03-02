5e94b16e2f83c1068be54a363cc108c8e967ef0b
Aleandro2119 <aleandro.basilico@gmail.com>
2022-03-02 18:18:25 +0100

    Add print in pages and censured the word

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $word = $_GET["word"];
 
 
 $censured = str_replace($word, '***', $paragraph);
+$censured_length = strlen($censured);
 
 ?>
 
@@ -21,11 +22,20 @@ $censured = str_replace($word, '***', $paragraph);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP-BADWORDS</title>
 </head>
 
 <body>
+    <main>
+        <h1><?php echo $censured ?></h1>
+        <h2><?php echo "Lunghezza paragrafo originale: $paragraph_length"; ?></h2>
+        <h2><?php echo "Lunghezza paragrafo censurato: $censured_length"; ?></h2>
 
+        <form method="get">
+            <input type="text" placeholder="Inserisci la parola da censurare" name="word">
+            <button type="submit">Censured</button>
+        </form>
+    </main>
 </body>
 
 </html>
